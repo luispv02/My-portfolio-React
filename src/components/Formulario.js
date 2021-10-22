eb2f4c1a2026e430d51c1c3ac679ffdc25b10912
Luis Fernando <luispv.1002@gmail.com>
2021-10-22 18:41:20 -0500

    Enviar Mensajes

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,16 +10,20 @@ function Formulario() {
                 <Row>
                     <Col md={10} lg={8} className="m-auto">
                         <h2 className="text-center">Contacto</h2>
-                        <form className="p-3">
+                        <form className="p-3" action="https://formsubmit.co/corre.pruebas10@gmail.com" method="POST" target="_blank" rel="noopener noreferrer">
+                             <div>
+                                <TextField className="w-100" label="Nombre" name="nombre"  variant="standard" required/>
+                            </div>
+
                             <div>
-                                <TextField className="w-100" label="Asunto" variant="standard" />
+                                <TextField className="w-100 mt-4" label="Asunto" name="asunto"  variant="standard" required/>
                             </div>
 
                             <div className="mt-4">
-                                <TextField className="w-100" label="Mensaje" variant="standard" />
+                                <TextField className="w-100" label="Mensaje" name="mensaje"  variant="standard" required/>
                             </div>
 
-                            <button>Enviar</button>
+                            <button type="submit">Enviar</button>
                         </form>
                     </Col>
                 </Row>
